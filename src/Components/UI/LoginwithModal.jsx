28274d1fc06a43d80bbe369f195dd74855291a14
suffynux <suffynux@gmail.com>
2025-10-05 17:59:31 +0500

    new layout

--- src/Components/UI/LoginwithModal.jsx
+++ src/Components/UI/LoginwithModal.jsx
@@ -8,7 +8,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "./dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 // import { Button } from "@/components/ui/button"
